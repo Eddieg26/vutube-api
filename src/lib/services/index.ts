ff70540f9f9734ddfe9a29afaec2fa788569f7d3
Eddie <eddie_0226@outlook.com>
2024-04-09 07:48:40 -0600

    video storage

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -3,27 +3,30 @@ import {Session, User} from '../models';
 import {Config} from '../types';
 import {Database} from './database';
 import {Redis} from './redis';
+import {VideoStorage} from './storage';
 
 export {Database, Redis};
 
 export type AppContext = {
   services: Services;
-};
+} & Koa.DefaultContext;
 
 export type AppState = {
   user?: User;
   session?: Session;
   config: Config;
-};
+} & Koa.DefaultState;
 
 export type Context = Koa.ParameterizedContext<AppState, AppContext>;
 
 export class Services {
   database: Database;
   redis: Redis;
+  videoStorage: VideoStorage;
 
   constructor(config: Config) {
     this.database = new Database(config.dbUrl);
     this.redis = new Redis();
+    this.videoStorage = new VideoStorage();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import 'dotenv/config';
 import bodyParser = require('koa-bodyparser');
 import {App, Config, authRouter, userRouter} from './lib';
 
-function main() {
+async function main() {
   const app = new App(new Config());
 
   app.use(bodyParser());
   app.router(userRouter);
   app.router(authRouter);
 
-  app.run().then(server => server.on('close', app.stop));
+  const server = await app.run();
+  server.on('close', app.stop.bind(app));
 }
 
 main();
